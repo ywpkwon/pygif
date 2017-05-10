062454962d40053b805c754153cd217909bd242a
Youngwook Paul Kwon <young@berkeley.edu>
2017-05-10 14:46:33 -0700

    filename sorting

--- pygif.py
+++ pygif.py
@@ -1,16 +1,22 @@
 import argparse
 import imageio
 import os
+import glob
 
 def makegif(inf, fps):
 
+    # sorted image file names
+    valid_ext = ["jpg","gif","png","tga"]
+    filenames = []
+    for ext in valid_ext:
+        filenames += glob.glob(os.path.join(inf, '*.'+ext))
+    filenames.sort()
+
     # Load each file into a list
-    frames = []
-    for root, dirs, filenames in os.walk(inf):
-      for filename in filenames:
-        if any([ext in filename.lower() for ext in [".png", ".jpg"]]):
-            print(filename)
-            frames.append(imageio.imread(os.path.join(inf,filename)))
+    frames = [] 
+    for filename in filenames:
+        print(filename)
+        frames.append(imageio.imread(filename))
 
     # Save them as frames into a gif
     exportname = "output.gif"
